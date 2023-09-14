73235f84717bbdc8e143c9ad6da657bf17505ef5
tinashe ota <tinasheota@gmail.com>
2023-09-14 16:18:25 -0500

    add: add another compenent to the layout

---
+++ components/ServicePro.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="text-black-100 py-12 px-6 md:flex justify-between items-center flex-col text-center">
+        <h2 class="text-4xl font-semibold mb-4">How It Works</h2>
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-4 justify-center items-center">
+            <div class="text-center md:px-20 px-5">
+                <div class="inline-flex">
+                <h3 class="text-gray-500 text-6xl mb-2 uppercase font-bold">01</h3>
+                <img class="w-16 h-16 mb-2" src="@/assets/icon1.png" alt="icon1">
+            </div>
+                <h4 class="text-lg font-semibold mb-2">Browse our amazing deals</h4>
+                <p class="text-gray-500">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia conseq uuntur magni dolores eos qui ratione.</p>
+            </div>
+            <div class="text-center md:px-20 px-5">
+                <div class="inline-flex">
+                <h3 class="text-gray-500 text-6xl  mb-2 uppercase font-bold">02</h3>
+                <img class= "w-16 h-16 mb-2" src="@/assets/icon2.png" alt="Icon 2">
+                </div>
+                <h4 class="text-lg font-semibold mb-2">Connect with an expert</h4>
+                <p class="text-gray-500">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia conseq uuntur magni dolores eos qui ratione.</p>
+            </div>
+            <div class="text-center md:px-20 px-5">
+                <div class="inline-flex">
+                <h3 class="text-gray-500 text-6xl  mb-2 uppercase font-bold">03</h3>
+                <img class="inline-block w-16 h-16 mb-2" src="@/assets/icon3.png" alt="Icon 03">
+            </div>
+                <h4 class="text-lg font-semibold mb-2">Enjoy your vacation</h4>
+                <p class="text-gray-500">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia conseq uuntur magni dolores eos qui ratione.</p>
+            </div>
+        </div>
+    </div>
+    
+    </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="flex flex-col min-h-screen">
         <div><MainNavigation/></div>
+        <div><ServicePro/></div>
         <div><CustomerService/></div>
         <div><FooterExample /></div>
     </div>
