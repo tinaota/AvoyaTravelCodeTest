aca4e9dc754674d6c720e276a9380bfa7ef83f65
tinashe ota <tinasheota@gmail.com>
2023-09-14 15:26:08 -0500

    fix: add font awesome for menu postiion mobile

--- components/MainNavigation.vue
+++ components/MainNavigation.vue
@@ -1,33 +1,36 @@
 <template>
-<div class="text-white-100 py-3.5 px-6 md:flex justify-between items-center bg-gray-100">
+    <div class="text-white-100 py-3.5 px-6 md:flex justify-between items-center bg-gray-100">
       <div class="flex items-center cursor-pointer">
-            <div><img src="@/assets/logo.png" alt="Main Logo" /></div>
+        <div><img src="@/assets/logo.png" alt="Main Logo" /></div>
       </div>
-      <span class="absolute md:hidden">
-        <i :class="[open ? 'bi bi-x' : 'bi bi-list']"></i>
+      <span class="absolute right-6 top-8 md:hidden cursor-pointer" @click="toggleMenu">
+        <font-awesome-icon :icon="open ? 'times' : 'bars'" />
       </span>
       <ul
-        class="md:flex md:items-center md:px-0 px-3 md:pb-0 md:p-10 md:static absolute bg-grey-900 md:w-auto w-full top-14 duration-700 ease-in"
+        class="md:flex md:items-center md:px-0 px-3 md:pb-0 md:p-10 md:static absolute md:bg-transparent bg-gray-100 text-white md:w-auto w-full top-14 duration-700 ease-in"
         :class="[open ? 'left-0' : 'left-[-100%]']"
       >
         <li class="md:mx-4" v-for="link in Links" :key="link.name">
-          <a :href="link.link" class="text-xl hover:text-orange-400">{{ link.name }}</a>
+          <a :href="link.link" class="text-xl hover:text-orange-400" @click="closeMenu">{{ link.name }}</a>
         </li>
-        <Button></Button>
+        <li><Button></Button></li>
       </ul>
     </div>
-
   </template>
   
   <script>
   import Button from './Button';
-  import { ref } from 'vue'; // Import ref
+  import { ref } from 'vue';
+  import { library } from '@fortawesome/fontawesome-svg-core';
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+  import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
   
+  library.add(faBars, faTimes);
   
   export default {
     components: {
       Button,
-
+      FontAwesomeIcon,
     },
     setup() {
       let open = ref(false);
@@ -41,7 +44,17 @@
         { name: "BLOG", link: "#" },
         { name: "GET STARTED", link: "#" },
       ];
-      return { Links, open }; // Return open so you can use it in your template
+  
+      const toggleMenu = () => {
+        open.value = !open.value;
+      };
+  
+      const closeMenu = () => {
+        open.value = false;
+      };
+  
+      return { Links, open, toggleMenu, closeMenu };
     },
   };
   </script>
+  
