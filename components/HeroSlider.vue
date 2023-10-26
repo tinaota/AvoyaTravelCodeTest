f9b6d8b41a067d80b15c5864278bde013365c9c3
tinaota <tinaota87@outlook.com>
2023-10-26 12:06:10 -0500

    update slider to fix images issues

--- components/HeroSlider.vue
+++ components/HeroSlider.vue
@@ -1,98 +1,66 @@
 <template>
-    <!--having problems with reuesting images with the data -->
-    <div class="slider">
-      <div class="slides">
-        <div v-for="(slide, index) in slides" :key="index" class="slide" :class="{ active: index === currentIndex }">
-            <img :src="slide.imageUrl" alt="Slide Image">
-            </div>
+  <Carousel>
+    <Slide v-for="slide in slides" :key="slide.id">
+      <div class="carousel__item">
+        <img :src="slide.src" alt="Slide {{ slide.id }}" class="carousel__image" />
       </div>
-      
-      <div class="controls">
-        <button @click="prevSlide">&laquo;</button>
-        <button @click="nextSlide">&raquo;</button>
-      </div>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        slides: [
-        { imageUrl: "@/assets/slide1.png" },
-        { imageUrl: "@/assets/slide2.png" },
-        { imageUrl: "@/assets/slide3.png" },
-        ],
-        currentIndex: 0,
-        intervalId: null,
-        intervalDuration: 3000, // Change slide every 5 seconds
-      };
-    },
-    methods: {
-      prevSlide() {
-        this.currentIndex = (this.currentIndex - 1 + this.slides.length) % this.slides.length;
-      },
-      nextSlide() {
-        this.currentIndex = (this.currentIndex + 1) % this.slides.length;
-      },
-      startAutoRotation() {
-        this.intervalId = setInterval(this.nextSlide, this.intervalDuration);
-      },
-      stopAutoRotation() {
-        clearInterval(this.intervalId);
-      },
-    },
-    created() {
-      this.startAutoRotation();
-    },
-    beforeDestroy() {
-      this.stopAutoRotation();
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .slider {
-    position: relative;
-    width: 100%;
-    max-width:100%;
-    margin: 0 auto;
-  }
-  
-  .slides {
-    display: flex;
-    transition: transform 0.5s ease-in-out;
-  }
-  
-  .slide {
-    flex: 0 0 100%;
-    padding: 20px;
-    text-align: center;
-    display: none;
-  }
-  
-  .slide.active {
-    display: block;
-  }
-  
-  .controls {
-    margin-top: 10px;
-    text-align: center;
-  }
-  
-  button {
-    margin: 5px;
-    padding: 10px 20px;
-    background-color: grey;
-    color: white;
-    border: none;
-    cursor: pointer;
-    border-radius: 20px;
-    background-color: ;
-  }
-  
-  button:hover {
-    background-color: white;
-    color:#000;
-  }
-  </style>
+    </Slide>
+
+    <template #addons>
+      <Navigation />
+      <Pagination />
+    </template>
+  </Carousel>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel'
+
+import 'vue3-carousel/dist/carousel.css'
+
+import slide1 from '/assets/slideshow/slide1.png'
+import slide2 from '/assets/slideshow/slide2.png'
+import slide3 from '/assets/slideshow/slide3.png'
+
+export default defineComponent({
+  name: 'Basic',
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
+  data() {
+    return {
+      slides: [
+        { id: 1, src: slide1 },
+        { id: 2, src: slide2 },
+        { id: 3, src: slide3 },
+      ]
+    }
+  },
+})
+</script>
+
+<style>
+.carousel__item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.carousel__image {
+  width: 100%;
+}
+
+.carousel__slide {
+  padding: 10px;
+}
+
+.carousel__prev,
+.carousel__next {
+  box-sizing: content-box;
+  border: 5px solid white;
+}
+</style>
